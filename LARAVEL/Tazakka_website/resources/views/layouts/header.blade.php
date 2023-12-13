5ed767f3494c4a4da9e57130b79ebe2eccb37b46
ilham Suryana <szuryanailham090102@gmail.com>
2023-12-13 20:48:11 +0700

    memperbaiki header

--- LARAVEL/Tazakka_website/resources/views/layouts/header.blade.php
+++ LARAVEL/Tazakka_website/resources/views/layouts/header.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    
   </head>
   <body>
         @include('partials.navbar')
